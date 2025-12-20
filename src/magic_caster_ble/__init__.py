496a74aea38e6ecd501d60eb5d0374b80ad87355
anon <anon@example.com>
2025-12-20 11:24:42 -0600

    fix: export consts for use in HA component

--- src/magic_caster_ble/__init__.py
+++ src/magic_caster_ble/__init__.py
@@ -5,11 +5,13 @@ __version__ = "1.1.7"
 
 from bleak_retry_connector import get_device
 
+from .const import WAND_FRIENDLY_TO_UUID
 from .exceptions import CharacteristicMissingError
 from .wand_ble import MagicCasterWandBLE
 
 __all__ = [
     "CharacteristicMissingError",
     "MagicCasterWandBLE",
+    "WAND_FRIENDLY_TO_UUID",
     "get_device",
 ]
